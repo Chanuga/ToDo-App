bbc5c5157faf8de80003e2a1f8db9c0cfaea9a93
Chanuga <Chanugatharindu1995@gmail.com>
2021-01-02 01:06:31 +0530

    Updates

---
+++ precache-manifest.a6ee3fa09e7fada23621509e91c8089a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659929b51a08fc16661410017bb92c1e",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "67744d8d65d6081dc5c9",
+    "url": "/ToDo-App/static/css/2.ef440c72.chunk.css"
+  },
+  {
+    "revision": "45b0d22b986460f2a2d8",
+    "url": "/ToDo-App/static/css/main.ec321a49.chunk.css"
+  },
+  {
+    "revision": "67744d8d65d6081dc5c9",
+    "url": "/ToDo-App/static/js/2.cf4369f0.chunk.js"
+  },
+  {
+    "revision": "45b0d22b986460f2a2d8",
+    "url": "/ToDo-App/static/js/main.5f862700.chunk.js"
+  },
+  {
+    "revision": "48123a90e4e1f78e1081",
+    "url": "/ToDo-App/static/js/runtime~main.b7561b79.js"
+  }
+]);
